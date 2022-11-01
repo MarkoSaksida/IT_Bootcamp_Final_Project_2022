833364211ee343ebc89dae4e73739507a96bb5d7
Marko Saksida <marko.saksida@icloud.com>
2022-11-01 11:58:03 +0100

    minor fixes

--- src/test/java/automationpractice/com/tests/ShopFlowTests.java
+++ src/test/java/automationpractice/com/tests/ShopFlowTests.java
@@ -7,7 +7,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-public class ShopWorkflowTests extends BaseTest {
+public class ShopFlowTests extends BaseTest {
 
     private HomePage homePage;
     private AccountCreateOrLogInPage accountCreateOrLogInPage;
